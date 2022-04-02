45049c23c74208c42aa1d559998d0cda9c7f6539
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2022-04-02 09:07:51 -0700

    Implemented UI buttons mouseover + click fx

---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixelplacement;
+
+public class UIButton : Interactable2D
+{
+    [SerializeField]
+    private SpriteRenderer buttonRenderer = default;
+
+    [SerializeField]
+    private Vector2 mouseOverOffset = default;
+
+    protected override void OnCursorEnter()
+    {
+        Tween.LocalPosition(buttonRenderer.transform, mouseOverOffset, 0.1f, 0f, Tween.EaseOut);
+    }
+
+    protected override void OnCursorExit()
+    {
+        Tween.LocalPosition(buttonRenderer.transform, Vector2.zero, 0.1f, 0f, Tween.EaseIn);
+    }
+
+    protected override void OnCursorSelectEnd()
+    {
+        buttonRenderer.enabled = false;
+        CustomCoroutine.WaitThenExecute(0.1f, () => buttonRenderer.enabled = true);
+        ScreenEffectsController.Instance.AddThenSubtractIntensity(ScreenEffect.RenderCanvasShake, 0.05f, 0.05f, 0.05f, 0.1f);
+    }
+}
