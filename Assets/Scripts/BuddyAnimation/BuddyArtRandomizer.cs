b8fb5b5d90f2b84daa2111fd34f6655f48da4b99
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2022-04-02 15:42:56 -0700

    Random buddy visual variations

---
+++ Assets/Scripts/BuddyAnimation/BuddyArtRandomizer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuddyArtRandomizer : ManagedBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer headRenderer = default;
+
+    [SerializeField]
+    private SpriteRenderer bodyRenderer = default;
+
+    [SerializeField]
+    private List<SpriteRenderer> coloredRenderers = default;
+
+    [SerializeField]
+    private List<Sprite> headSprites = default;
+
+    [SerializeField]
+    private List<Sprite> bodySprites = default;
+
+    [SerializeField]
+    private List<Color> spriteColors = default;
+
+    private void Start()
+    {
+        Randomize();
+    }
+
+#if UNITY_EDITOR
+    public override void ManagedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Randomize();
+        }
+    }
+#endif
+
+    private void Randomize()
+    {
+        headRenderer.sprite = headSprites[Random.Range(0, headSprites.Count)];
+        bodyRenderer.sprite = bodySprites[Random.Range(0, bodySprites.Count)];
+        Color randomColor = spriteColors[Random.Range(0, spriteColors.Count)];
+        coloredRenderers.ForEach(x => x.color = randomColor);
+    }
+}
