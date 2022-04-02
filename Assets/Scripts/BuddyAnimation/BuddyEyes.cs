8cfb162a6b7a796186f5cd6eea6ade474dfb1708
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2022-04-01 22:07:32 -0700

    Buddy eyes / mouth scripts

--- Assets/Scripts/BuddyAnimation/BuddyEyes.cs
+++ Assets/Scripts/BuddyAnimation/BuddyEyes.cs
@@ -1,6 +1,54 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class BuddyEyes : ManagedBehaviour
+public class BuddyEyes : TimedBehaviour
 {
-    
+    [SerializeField]
+    private float doubleBlinkChance = 0.2f;
+
+    [SerializeField]
+    private List<Sprite> openSprites = default;
+
+    [SerializeField]
+    private List<Sprite> closedSprites = default;
+
+    private Emotion currentEmotion = Emotion.Neutral;
+    private bool eyesClosed = false;
+
+    public void SetEmotion(Emotion emotion)
+    {
+        currentEmotion = emotion;
+        UpdateSprite();
+    }
+
+    protected override void OnTimerReached()
+    {
+        StartCoroutine(BlinkSequence());
+    }
+
+    private IEnumerator BlinkSequence()
+    {
+        yield return Blink();
+        if (Random.value < doubleBlinkChance)
+        {
+            yield return Blink();
+        }
+    }
+
+    private IEnumerator Blink()
+    {
+        eyesClosed = true;
+        UpdateSprite();
+        yield return new WaitForSeconds(0.1f);
+        eyesClosed = false;
+        UpdateSprite();
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    private void UpdateSprite()
+    {
+        var sprites = eyesClosed ? closedSprites : openSprites;
+        GetComponent<SpriteRenderer>().sprite = sprites[(int)currentEmotion - 1];
+    }
 }
--- Assets/Scripts/BuddyAnimation/BuddyMouth.cs
+++ Assets/Scripts/BuddyAnimation/BuddyMouth.cs
@@ -1,6 +1,33 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class BuddyMouth : ManagedBehaviour
 {
-    
+    [SerializeField]
+    private List<Sprite> openSprites = default;
+
+    [SerializeField]
+    private List<Sprite> closedSprites = default;
+
+    private Emotion currentEmotion = Emotion.Neutral;
+    private bool mouthClosed = false;
+
+    public void SetOpen(bool open)
+    {
+        mouthClosed = !open;
+        UpdateSprite();
+    }
+
+    public void SetEmotion(Emotion emotion)
+    {
+        currentEmotion = emotion;
+        UpdateSprite();
+    }
+
+    private void UpdateSprite()
+    {
+        var sprites = mouthClosed ? closedSprites : openSprites;
+        GetComponent<SpriteRenderer>().sprite = sprites[(int)currentEmotion - 1];
+    }
 }
