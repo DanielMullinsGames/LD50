95de63020592886d075675d0c9a5d0b3a6be428e
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2022-04-02 09:19:01 -0700

    SFX for buttons. Enable buttons after intro dialogue

--- Assets/Scripts/StoryProgress/ProgressionSequencer.cs
+++ Assets/Scripts/StoryProgress/ProgressionSequencer.cs
@@ -7,6 +7,9 @@ public class ProgressionSequencer : MonoBehaviour
     [SerializeField]
     private List<DialogueEvent> introEvents = default;
 
+    [SerializeField]
+    private List<UIButton> initialButtons = default;
+
     private void Start()
     {
         StartCoroutine(IntroSequence());
@@ -14,7 +17,13 @@ public class ProgressionSequencer : MonoBehaviour
 
     IEnumerator IntroSequence()
     {
+        initialButtons.ForEach(x => x.SetHidden());
         yield return new WaitForSeconds(1f);
         yield return DialogueHandler.Instance.PlayDialogueEvent(introEvents[0]);
+        foreach (var button in initialButtons)
+        {
+            button.Show();
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
--- Assets/Scripts/UI/UIButton.cs
+++ Assets/Scripts/UI/UIButton.cs
@@ -11,9 +11,23 @@ public class UIButton : Interactable2D
     [SerializeField]
     private Vector2 mouseOverOffset = default;
 
+    public void SetHidden()
+    {
+        buttonRenderer.transform.localPosition = new Vector2(mouseOverOffset.x * -10f, 0f);
+        SetCollisionEnabled(false);
+    }
+
+    public void Show()
+    {
+        Tween.LocalPosition(buttonRenderer.transform, Vector2.zero, 0.15f, 0f, Tween.EaseIn);
+        AudioController.Instance.PlaySound2D("button_mouseover", 0.5f, pitch: new AudioParams.Pitch(AudioParams.Pitch.Variation.Small));
+        SetCollisionEnabled(true);
+    }
+
     protected override void OnCursorEnter()
     {
         Tween.LocalPosition(buttonRenderer.transform, mouseOverOffset, 0.1f, 0f, Tween.EaseOut);
+        AudioController.Instance.PlaySound2D("button_mouseover", 0.5f, pitch: new AudioParams.Pitch(AudioParams.Pitch.Variation.Small));
     }
 
     protected override void OnCursorExit()
@@ -26,5 +40,6 @@ public class UIButton : Interactable2D
         buttonRenderer.enabled = false;
         CustomCoroutine.WaitThenExecute(0.1f, () => buttonRenderer.enabled = true);
         ScreenEffectsController.Instance.AddThenSubtractIntensity(ScreenEffect.RenderCanvasShake, 0.05f, 0.05f, 0.05f, 0.1f);
+        AudioController.Instance.PlaySound2D("button_press_chunk", 1f, pitch: new AudioParams.Pitch(AudioParams.Pitch.Variation.Small));
     }
 }
