395309ba8f12316df7a4dcbfbc552a511d2c0f3d
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2022-04-01 21:29:02 -0700

    Scrolling bg art + effect

---
+++ Assets/Addons/DanielMullinsGames/Animation/ScrollUV.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+public class ScrollUV : MonoBehaviour
+{
+
+	[SerializeField]
+	private Vector2 startOffset = default;
+
+    [SerializeField]
+    private int materialIndex = 0;
+
+	public Vector2 scrollVector;
+	public float scrollSpeed;
+
+    private Renderer mainRenderer;
+
+	void Start()
+	{
+        mainRenderer = GetComponent<Renderer>();
+
+        if (HasProperty())
+        {
+            mainRenderer.materials[materialIndex].mainTextureOffset = startOffset;
+        }
+        else
+        {
+            enabled = false;
+        }
+	}
+
+	void Update()
+    {
+        if (HasProperty())
+        {
+            mainRenderer.materials[materialIndex].mainTextureOffset += scrollVector * scrollSpeed * Time.deltaTime;
+        }
+	}
+
+    private bool HasProperty()
+    {
+        return mainRenderer.materials[materialIndex].HasProperty("_MainTex");
+    }
+}
