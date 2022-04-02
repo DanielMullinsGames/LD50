f9e2149c42c89d8ba2ce29a992e633a8c0c165d5
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2022-04-02 10:26:27 -0700

    Art for food + hat. Food spawning

---
+++ Assets/Scripts/Gameplay/FoodSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : ManagedBehaviour
+{
+    [SerializeField]
+    private List<GameObject> foodPrefabs = default;
+
+    public void SpawnFood()
+    {
+        var obj = Instantiate(foodPrefabs[Random.Range(0, foodPrefabs.Count)]);
+        obj.transform.position = transform.position;
+        obj.transform.SetParent(transform);
+
+        obj.GetComponent<Rigidbody2D>().AddForce(Vector2.right * CustomRandom.RandomBetween(-50f, 50f));
+    }
+}
