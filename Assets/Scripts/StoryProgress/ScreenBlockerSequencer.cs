337ae83a298cefbd1f549e5a65fce344c4448b8e
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2022-04-03 11:28:13 -0700

    Blackout screen sequence

---
+++ Assets/Scripts/StoryProgress/ScreenBlockerSequencer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBlockerSequencer : Sequencer
+{
+    [SerializeField]
+    private GameObject screenBlocker = default;
+
+    private bool mouseExitReentered = false;
+
+    protected override IEnumerator Sequence()
+    {
+        screenBlocker.SetActive(true);
+        float startTime = Time.time;
+
+        StartCoroutine(WaitForMouseExitReenter());
+        yield return new WaitUntil(() => Time.time - startTime > 10f || mouseExitReentered);
+
+        screenBlocker.SetActive(false);
+
+        DialogueHandler.Instance.AddDialogueEventToStack(dialogueEvents[0]);
+        yield return new WaitUntil(() => DialogueHandler.Instance.NoDialoguePlaying);
+    }
+
+    private IEnumerator WaitForMouseExitReenter()
+    {
+        yield return new WaitUntil(() => !MousePosChecker.MouseOverGameWindow);
+        yield return new WaitUntil(() => MousePosChecker.MouseOverGameWindow);
+        mouseExitReentered = true;
+    }
+}
