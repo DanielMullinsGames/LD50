8d7fa5aa5c501506ef84128eabd6b560a88d97f7
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2022-04-02 19:15:19 -0700

    Progress on rowboat scene

---
+++ Assets/Scripts/StoryProgress/RowboatSequencer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowboatSequencer : Sequencer
+{
+    [SerializeField]
+    private UIButton holidayButton = default;
+
+    [SerializeField]
+    private GameObject rowboatScene = default;
+
+    private bool buttonPressed;
+
+    protected override IEnumerator Sequence()
+    {
+        yield return new WaitForSeconds(0.5f);
+        holidayButton.Show();
+
+        yield return new WaitUntil(() => buttonPressed);
+        rowboatScene.SetActive(true);
+
+        holidayButton.gameObject.SetActive(false);
+    }
+
+    public void OnButtonPressed()
+    {
+        buttonPressed = true;
+    }
+}
