be34cf5241baf7a4a219aeedb9ea060411dbbf4d
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2022-04-02 20:13:01 -0700

    Progress on ball throwing minigame

---
+++ Assets/Scripts/Gameplay/BallThrower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallThrower : TimedBehaviour
+{
+    [SerializeField]
+    private Animator throwerAnim = default;
+
+    [SerializeField]
+    private GameObject ballObj = default;
+
+    [SerializeField]
+    private Transform handTarget = default;
+
+    protected override void OnTimerReached()
+    {
+        StartThrow();
+    }
+
+    private void StartThrow()
+    {
+        BuddyHandsController.Instance.SetLeftHandTarget(handTarget);
+        throwerAnim.Play("throw", 0, 0f);
+    }
+
+    public void OnReleaseKeyframe()
+    {
+        var newBall = Instantiate(ballObj);
+        newBall.SetActive(true);
+        newBall.transform.position = ballObj.transform.position;
+        newBall.GetComponent<Rigidbody2D>().AddForce(Vector2.up * CustomRandom.RandomBetween(150f, 200f) + Vector2.right * CustomRandom.RandomBetween(-5f, 100f));
+    }
+}
